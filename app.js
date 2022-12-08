eff9bc78df2f2647e2f47b56bedf2003d0bd6838
mac <mac@MacBook-Air-mac.local>
2022-12-08 23:56:59 +0200

    lesson-12 - done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let res = revenue + bonus;
-console.log(res);
+function getFullName(firstname, surname) {
+    if (typeof firstname !== 'string') {
+        throw new Error('bad');
+    }
+    return `${firstname} ${surname}`;
+}
+console.log(getFullName(true, false));
--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
 function getFullName(firstname: string, surname: string): string {
+    if (typeof firstname !== 'string') {
+        throw new Error('bad');
+    }
+
     return `${firstname} ${surname}`;
 }
 
- 
+console.log(getFullName('true', 'false'));
