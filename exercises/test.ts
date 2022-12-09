ddfa77720f0a9b518e52522cdf61535c4c7bcaf4
mac <mac@MacBook-Air-mac.local>
2022-12-09 17:18:17 +0200

    lesson-14 - done

---
+++ exercises/test.ts
@@ -0,0 +1,21 @@
+let info: {
+    officeId: number;
+    isOpened: boolean;
+    contacts: {
+        phone: string;
+        email: string;
+        address: {
+            city: string
+        }
+    }
+} = {
+	"officeId": 45,
+	"isOpened": false,
+	"contacts": {
+		"phone": "+380964335192",
+		"email": "some@gmail.com",
+		"address": {
+			"city": "Kyiv"
+		}
+	}
+}
