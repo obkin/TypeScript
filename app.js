31f862126b08760f8c364e0449e3a407eee8f1b8
obkin <kapellwork@gmail.com>
2022-12-20 10:37:55 +0200

    lesson-18 - done

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 "use strict";
-function getFullName(firstname, surname) {
-    if (typeof firstname !== 'string') {
-        throw new Error('bad');
-    }
-    return `${firstname} ${surname}`;
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 200] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 201] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 202] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Payment was success',
+    statusCode: 201
+};
+if (res.statusCode === StatusCode.SUCCESS) {
+    console.log('Successful status!');
 }
-console.log(getFullName(true, false));
+else if (res.statusCode === StatusCode.IN_PROCESS) {
+    console.log('In process...');
+}
+else if (res.statusCode === StatusCode.FAILED) {
+    console.log('Failed status.');
+}
+else {
+    console.log('Unknown status :/');
+}
+// 1 - success
+// 2 - in process
+// 3 - was failed
