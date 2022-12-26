db2bcd6827328eb1250dd136201fe62e096672f6
obkin <kapellwork@gmail.com>
2022-12-26 22:09:02 +0200

    lesson-21 - done

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 "use strict";
 function logId(id) {
     if (typeof id === 'string') {
-        console.log(id.toLowerCase() + ' - string');
+        console.log(id.toLowerCase());
     }
     else if (typeof id === 'number') {
-        console.log(id + ' - number');
+        console.log('0' + id);
     }
     else {
-        console.log(id + ' - boolean');
+        console.log(`CAPS ON: ${id}`);
     }
 }
+logId('SOMEstr');
 logId(1);
-logId('Yarik');
-logId(true);
+logId(false);
--- app.ts
+++ app.ts
@@ -1,13 +1,49 @@
-function logId(id: string | number | boolean) {
+
+function logId(id: (string | number | boolean)) {
     if (typeof id === 'string') {
-        console.log(id.toLowerCase() + ' - string');
-    } else if (typeof id === 'number') {
-        console.log(id + ' - number');
+        console.log(id);
     } else {
-        console.log(id + ' - boolean');
+        console.log(id);
+    }
+};
+
+logId('someStr');
+logId(1);
+logId(false);
+
+
+
+function logError(err: (string | string[])) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    } else {
+        console.log(err);
+    }
+};
+
+
+
+function logObj(obj: { a: number } | { b: number }) {
+    if ('a' in obj) {
+        console.log(obj.a);
+    } else {
+        console.log(obj.b);
     }
 }
 
-logId(1);
-logId('Yarik');
-logId(true);
+
+
+function logMultipliesId(a: string | number, b: string | boolean) {
+    if (a === b) {
+        console.log(a.toUpperCase);
+        console.log(b.toLowerCase);
+    } else {
+        console.log(a);
+        console.log(b);
+    }
+}
+
+
+
+let a: 12 = 12;
+
