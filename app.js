be50a8a92c4c6c25954b14c45cdd0a3255149b2e
mac <mac@MacBook-Air-mac.local>
2023-01-03 15:23:41 +0200

    lesson-23 - 3/4

--- app.js
+++ app.js
@@ -1,50 +1,6 @@
 "use strict";
-function logId(id) {
-    if (typeof id === 'number') {
-        console.log(0 + `${id} - is a number.`);
-    }
-    else {
-        console.log(typeof id);
-    }
+function fetchWithAuth(url, method) {
+    return 1;
 }
-logId(1);
-logId('str');
-logId(false);
-function logError(err) {
-    if (Array.isArray(err)) {
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-function logObj(obj) {
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-}
-logObj({ b: 5 });
-logObj({ a: 2 });
-function logMultipliesId(a, b) {
-    if (a === b) {
-        console.log(a, b);
-    }
-    else {
-        console.log(a);
-        console.log(b);
-    }
-}
-let a = 'sTr';
-function someFunc(obj) {
-    if ('a' in obj) {
-        console.log(obj.a.toLowerCase());
-    }
-    else {
-        console.log(obj.b + 10);
-    }
-}
-someFunc({ a: 'STrinG' });
-someFunc({ b: 5 });
+fetchWithAuth('htpp://youtube.com', 'GET');
+
--- app.ts
+++ app.ts
@@ -1,62 +1,23 @@
-
-function logId(id: number | string | boolean) {
-    if (typeof id === 'number') {
-        console.log(0 + `${id} - is a number.`)
-    } else {
-        console.log(typeof id);
-    }
+type User = {
+    name: string,
+    age: number,
+    skills: string[]
 }
 
-logId(1);
-logId('str');
-logId(false);
-
-
-
-
-function logError(err: string | string[] ) {
-    if (Array.isArray(err)) {
-        console.log(err);
-    } else {
-        console.log(err);
-    }
+type Role = {
+    name: string,
+    id: number;
 }
 
+type UserWithRole = {
+    user: User,
+    role: Role
+};
 
+let user: UserWithRole = {
+    name: 'Yarik',
+    age: 19,
+    skills: ['1', '2'],
+    id: 9
+};
 
-function logObj(obj: { a: number } | { b: number }) {
-    if ('a' in obj) {
-        console.log(obj.a);
-    } else {
-        console.log(obj.b);
-    }
-}
-
-logObj({b: 5});
-logObj({a: 2});
-
-
-function logMultipliesId(a: string | number, b: string | boolean) {
-    if (a === b) {
-        console.log(a, b);
-    } else {
-        console.log(a);
-        console.log(b);
-    }
-}
-
-
-
-let a: 'sTr' = 'sTr';
-
-
-function someFunc(obj: { a: string } | { b: number }) {
-    if ('a' in obj) {
-        console.log(obj.a.toLowerCase());
-    } else {
-        console.log(obj.b + 10);
-    }
-}
-
-someFunc({a: 'STrinG'});
-someFunc({b: 5});
