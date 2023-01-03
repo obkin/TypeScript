6a3d3cb54fe383315a6ee1dbe86116fd485e991d
obkin <kapellwork@gmail.com>
2023-01-03 12:24:39 +0200

    lesson-21 - repeated

--- app.ts
+++ app.ts
@@ -1,16 +1,62 @@
 
+function logId(id: number | string | boolean) {
+    if (typeof id === 'number') {
+        console.log(0 + `${id} - is a number.`)
+    } else {
+        console.log(typeof id);
+    }
+}
+
+logId(1);
+logId('str');
+logId(false);
 
 
 
 
-function fetchWithAuth(url: string, method: 'GET' | 'POST'): string {
-    return 'str';
-};
+function logError(err: string | string[] ) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    } else {
+        console.log(err);
+    }
+}
 
-fetchWithAuth('https://youtube.com', 'GET');
+
+
+function logObj(obj: { a: number } | { b: number }) {
+    if ('a' in obj) {
+        console.log(obj.a);
+    } else {
+        console.log(obj.b);
+    }
+}
+
+logObj({b: 5});
+logObj({a: 2});
+
+
+function logMultipliesId(a: string | number, b: string | boolean) {
+    if (a === b) {
+        console.log(a, b);
+    } else {
+        console.log(a);
+        console.log(b);
+    }
+}
 
 
 
-let method = 'notGET';
+let a: 'sTr' = 'sTr';
 
-fetchWithAuth('https://youtube.com', method as 'GET');
+
+function someFunc(obj: { a: string } | { b: number }) {
+    if ('a' in obj) {
+        console.log(obj.a.toLowerCase());
+    } else {
+        console.log(obj.b + 10);
+    }
+}
+
+someFunc({a: 'STrinG'});
+someFunc({b: 5});
