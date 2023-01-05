414c1de98a1e462e17950dc27dfd90223930b68e
mac <mac@MacBook-Air-mac.local>
2023-01-05 08:50:38 +0200

    lesson-23 - repeat

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 "use strict";
-function fetchWithAuth(url, method) {
+function fetchWidthAuth(url, method) {
     return 1;
 }
-fetchWithAuth('htpp://youtube.com', 'GET');
-
+fetchWidthAuth('http://youtube.com', 'POST');
+// interface AboutCompany {
+//     name: string,
+//     capital: string,
+//     age: number,
+//     owners: string[]
+// };
+let company = {
+    info: {
+        name: "DREAM",
+        capital: '$' + 43876,
+        age: 7,
+        owners: ['Udyanskiy Mykola', 'Dmitriy Gordon', 'Petro Poroshenko']
+    },
+    id: {
+        name: "DREAM",
+        compID: 98134
+    }
+};
+console.log(company.info.capital);
--- app.ts
+++ app.ts
@@ -1,28 +1,53 @@
-type User = {
+type httpMethod = 'GET' | 'POST';
+
+type coolString = string;
+
+function fetchWidthAuth(url: coolString, method: httpMethod): 1 | -1 {
+    return 1;
+}
+
+fetchWidthAuth('http://youtube.com', 'POST');
+
+//
+
+
+type CompanyInfo = {
     name: string,
+    capital: string,
     age: number,
-    skills: string[]
-}
-
-type Role = {
-    name: string,
-    id: number;
-}
-
-type UserWithRole = {
-    user: User,
-    role: Role
+    owners: string[]
 };
 
-let user: UserWithRole = {
-    user: {
-        name: 'Yarik',
-        age: 19,
-        skills: ['Front-end', 'Back-end']
+type CompanyID = {
+    name: string,
+    compID: number
+};
+
+type CompanyWithID = {
+    info: CompanyInfo,
+    id: CompanyID
+}
+
+
+// interface AboutCompany {
+//     name: string,
+//     capital: string,
+//     age: number,
+//     owners: string[]
+// };
+
+let company: CompanyWithID = {
+    info: {
+        name: "DREAM",
+        capital: '$' + 43_876,
+        age: 7,
+        owners: ['Udyanskiy Mykola', 'Dmitriy Gordon', 'Petro Poroshenko']
     },
-    role: {
-        name: 'Developer',
-        id: 777
+
+    id: {
+        name: "DREAM",
+        compID: 98134
     }
 };
 
+console.log(company.info.capital);
