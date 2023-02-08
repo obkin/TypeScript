e9ca939f73a3951554e3342ec7c025c65c995a6b
mac <mac@MacBook-Air.local>
2023-02-08 19:07:16 +0200

    lesson-34 - done

--- app.ts
+++ app.ts
@@ -1,14 +1,3 @@
-const a = 5;
-const b:string = a.toString();
-
-let e: string = new String(a).valueOf();
-let f: boolean = new Boolean(a).valueOf();
-
-let c = '1';
-let d: number = parseInt(c);
-
-// ---------
-
 interface User {
     name: string;
     email: string;
@@ -26,12 +15,54 @@ const user: User = {
     login: 'vasya_vasya'
 }
 
-const admin = userToAdmin(user);
-
-function userToAdmin(user: User): Admin {
-    return {
-        name: user.name,
-        role: 1
+function logId(id: string | number) {
+    if (isString(id)) {
+        console.log(id);
+    } else {
+        console.log(id);
     }
 }
+
+// ---------
+
+function isString(x: string | number): x is string {
+    return typeof x === 'string';
+}
+
+// ---------
+
+function isAdmin(user: User | Admin): user is Admin {
+    return 'role' in user;
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+    return (user as Admin).role !== undefined;
+}
+
+function setRoleZero(user: User | Admin) {
+    if (isAdmin(user)) {
+        user.role = 0;
+    } else {
+        throw new Error(`${user} is not Admin`);
+    }
+}
+
+// ---------
+
+/*
+
+function func(a: string | number) {
+    if (isElemString(a)) {
+        console.log(a);
+    } else if (typeof a === 'number') {
+        console.log(a);
+    }
+    console.log(a);
+}
+
+function isElemString(el: unknown): el is string {
+    return el === 'string';
+}
+
+*/
 
